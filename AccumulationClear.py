65f8b1cb4d8465e09c933da167ef1ede240ffbdf
BalancedTernary <70513163+BalancedTernary@users.noreply.github.com>
2024-04-16 11:45:29 +0800

    大规模升级,注意用法已经改变

--- AccumulationClear.py
+++ AccumulationClear.py
@@ -5,24 +5,19 @@ import torch
 from torch.optim.optimizer import Optimizer, required
 
 #此优化器的主要超参数调节方法
-#1.设定resistance为None,lr_supplement为0,weight_decay为0
-#2.寻找一个可以持续训练,不频繁发散的最大lr
-#3.逐步调大resistance,寻找不破坏训练的最大resistance
-#4.逐步调大lr_supplement,寻找不破坏训练的最大lr_supplement
-#5.把resistance设置为1
-#6.适当调大weight_decay,直到训练稳定度不再变优的最小weight_decay
-#7.恢复resistance
-#8.依据损失下降曲线设定其他参数
+#1.
 
 class AccumulationClear(Optimizer):
-    def __init__(self, params, lr=1e-4, resistance=None, lr_supplement=1e-1, weight_decay=0, decay_flexibility=1, smoots_decrement=1e-2, snapshot_recovery_threshold=100, limit_snapshot_cycle=100, lossNeverNegative=True):
+    def __init__(self, params, lr=1e-2, smooth=10, lr_supplement=1e-2, soft_start=None, weight_decay=0, decay_flexibility=1, smoots_decrement=1e-2, snapshot_recovery_threshold=100, limit_snapshot_cycle=100, lossNeverNegative=True, deburr=False, nap=False, eps=1e-18):
         #parameter checks
         if lr<0: #学习速率
             raise ValueError(f'Invalid 学习速率: {lr}')
-        if resistance is not None and (resistance<0 or resistance>1): #阻力
-            raise ValueError(f'Invalid 刹车力度的倒数: {resistance}')
-        if lr_supplement is not None and (lr_supplement<0): #学习率下限
+        if smooth is not None and smooth<0: #平滑程度
+            raise ValueError(f'Invalid 平滑程度: {smooth}')
+        if lr_supplement is not None and (lr_supplement<0): #学习率补充
             raise ValueError(f'Invalid 学习率补充: {lr_supplement}')
+        if soft_start is not None and (soft_start<=0): #软起动,这个参数将乘在lr上，且每运行一次开一次方，越来越接近1.初始值越小，启动越软
+            raise ValueError(f'Invalid 软起动: {soft_start}')
         if weight_decay<0 or weight_decay>=1: #权重衰减
             raise ValueError(f'Invalid 权重衰减: {weight_decay}')
         if decay_flexibility<0: #权重衰减柔性
@@ -33,16 +28,23 @@ class AccumulationClear(Optimizer):
             raise ValueError(f'Invalid 快照恢复阈值: {snapshot_recovery_threshold}')
         if limit_snapshot_cycle<1: #快照周期下限
             raise ValueError(f'Invalid 快照周期下限: {limit_snapshot_cycle}')
+        if deburr and nap: #去除毛刺和加强毛刺
+            raise ValueError(f'Invalid 同时加强毛刺和去除毛刺是无效的:deburr and nap')
+        if eps<=0 or eps>=1: 
+            raise ValueError(f'Invalid eps:{eps}')
 
         if lr_supplement is None:
             lr_supplement=lr
+        if soft_start is None:
+            soft_start=smooth
 
-        defaults = dict(lr=lr, resistance=resistance, lr_supplement=lr_supplement, weight_decay=weight_decay, decay_flexibility=decay_flexibility, smoots_decrement=smoots_decrement, snapshot_recovery_threshold=snapshot_recovery_threshold, limit_snapshot_cycle=limit_snapshot_cycle, lossNeverNegative=lossNeverNegative)
+        defaults = dict(lr=lr, smooth=smooth, lr_supplement=lr_supplement, soft_start=soft_start, weight_decay=weight_decay, decay_flexibility=decay_flexibility, smoots_decrement=smoots_decrement, snapshot_recovery_threshold=snapshot_recovery_threshold, limit_snapshot_cycle=limit_snapshot_cycle, lossNeverNegative=lossNeverNegative, deburr=deburr, eps=eps)
         super(AccumulationClear, self).__init__(params,defaults)
 
         self.lr = lr
-        self.resistance = resistance
+        self.smooth = smooth
         self.lr_supplement=lr_supplement
+        self.soft_start=soft_start
         self.weight_decay = weight_decay
         self.decay_flexibility=decay_flexibility
         self.smoots_decrement=smoots_decrement
@@ -55,9 +57,14 @@ class AccumulationClear(Optimizer):
         self.upperEnvelopeCache = -2**15
         self.lowerEnvelopeCache = 2**15
         self.minNeutral = 2**15 #上包络与下包络的平均值的最小值
-        self.t=limit_snapshot_cycle-1 #计数器,拍摄快照后清零
+        self.t=0 #计数器,拍摄快照后清零
         self.maxAbsData=1
-        self.lossNeverNegative=lossNeverNegative
+        self.lossNeverNegative=lossNeverNegative #声明Loss值域是否非负
+        #可以用新旧值差异绝对值乘以self.smooth以抑制毛刺.但对于毛糙的曲面,这样可能会妨碍对全局最优解的寻找
+        self.deburr=deburr #是否去毛刺
+        self.nap=nap #是否加强毛刺
+        self.eps=eps
+        
 
     def __setstate__(self, state):
         super(AccumulationClear, self).__setstate__(state)
@@ -70,9 +77,16 @@ class AccumulationClear(Optimizer):
             loss=torch.tensor(1)
         
         k=torch.atan(loss*math.pi/2)*2/math.pi
+        b=torch.atan(loss/self.lr_supplement*math.pi/2)*2/math.pi
         if loss<0 or not self.lossNeverNegative:
             k=(k+1)/2
+            b=(b+1)/2
         k=1-torch.sqrt(1-k**2)    
+        b=1-torch.sqrt(1-b**2)    
+        s=1-1/(self.t/self.soft_start+1)
+        k=k*self.lr*s
+        b=b*self.lr*s*self.lr_supplement
+        
 
         self.t+=1
 
@@ -103,49 +117,65 @@ class AccumulationClear(Optimizer):
                     
                     state = self.state[p]  #get state dict for this param
                     if len(state) == 0:   #if first time to run...init dictionary with our desired entries
-                        state['speed'] = torch.zeros_like(p.grad.data)
+                        state['flat'] = torch.zeros_like(p.grad.data)
+                        state['absFlat'] = torch.zeros_like(p.grad.data)
                         state['snapshot'] = p.data.clone()
                     
                     if self.t>=self.limit_snapshot_cycle and neutral<self.minNeutral: #不能在这里清零t,否则快照拍摄不完整
                         state['snapshot'] = p.data.clone()
 
-                    #d=-p.grad.data*self.lr*(p.data.abs()+1)
                     maxAbsData0=max(maxAbsData0,p.data.abs().max())
+                    deburr=1
+                    if self.deburr:
+                        deburr=(state['flat']-p.grad.data).abs()
+                    nap=1
+                    if self.nap:
+                        nap=(state['flat']-p.grad.data).abs()
+                    #可以用新旧值差异绝对值乘以self.smooth以抑制毛刺.但对于毛糙的曲面,这样可能会妨碍对全局最优解的寻找
+                    state['flat']=(state['flat']*self.smooth*deburr+p.grad.data*nap)/(self.smooth*deburr+nap)
+                    state['flat'][~torch.isfinite(state['flat'])]=0
+                    state['absFlat']=(state['absFlat']*self.smooth*deburr+p.grad.data.abs()*nap)/(self.smooth*deburr+nap)
+                    state['absFlat'][~torch.isfinite(state['absFlat'])]=0
+                    
+                    #d=-p.grad.data*self.lr*(p.data.abs()+1)
                     #d=-p.grad.data*self.lr*((p.data.abs()/self.maxAbsData)*(loss-self.minLoss)/(self.maxLoss-self.minLoss)+self.lr_supplement)
                     #d=-p.grad.data*self.lr*((p.data.abs()/self.maxAbsData)+self.lr_supplement)
-                    d=-p.grad.data.sign()*torch.rand_like(p.grad.data)*k*self.lr*((p.data.abs()/self.maxAbsData)+self.lr_supplement)
-                    if self.resistance is None:
-                        state['speed'][state['speed']*d<0]=float('nan')
-                    else:
-                        state['speed'][(state['speed']+d)*d<0]*=self.resistance
+                    #d=-p.grad.data.sign()*torch.rand_like(p.grad.data)*k*self.lr*((p.data.abs()/self.maxAbsData)+self.lr_supplement)
+                    rand=torch.rand_like(p.grad.data)
+                    rand=1-(1-(rand**((state['absFlat'].abs()+self.eps)/(state['flat'].abs()+self.eps))))**((state['flat'].abs()+self.eps)/(state['absFlat'].abs()+self.eps))
+                    rand.clamp_(min=0,max=1)
+                    d=-state['flat'].sign()*rand*(k*(p.data.abs()/self.maxAbsData)+b)
                     d[~torch.isfinite(d)]=0
-                    state['speed']+=d
-                    decayMask=state['speed']*p.data>0
-                    x=state['speed'][decayMask]
-                    state['speed'][decayMask]+=(-2*self.weight_decay*x*(x/self.decay_flexibility).atan().abs())/math.pi
+                    
+                    #对使参数远离0的训练,进行抑制,实现权重衰减.
+                    decayMask=d*p.data>0
+                    x=d[decayMask]
+                    d[decayMask]+=(-2*self.weight_decay*x*(x/self.decay_flexibility).atan().abs())/math.pi
                     #state['speed'][decayMask]*=(1-self.weight_decay)
-                    state['speed'][~torch.isfinite(state['speed'])]=0
+                    d[~torch.isfinite(d)]=0
 
+                    #置当前步跨越0的参数和对应的平滑梯度为0,
                     #p.data.add_(state['speed'])
-                    buffer=p.data.add(state['speed'])
+                    buffer=p.data.add(d)
                     clearMask=buffer*p.data<0
                     buffer[clearMask]=0
                     p.data=buffer.data
-                    state['speed'][clearMask]=0
+                    state['flat'][clearMask]=0
 
                     if neutral>self.snapshot_recovery_threshold*self.minNeutral or not loss.isfinite():
-                        state['speed'] = torch.zeros_like(p.grad.data)
+                        state['flat'] = torch.zeros_like(p.grad.data)
+                        state['absFlat'] = torch.zeros_like(p.grad.data)
                         p.grad.data = torch.zeros_like(p.grad.data)
                         p.data=state['snapshot'].clone()
                         
         self.maxAbsData=maxAbsData0                
                     
         if self.t>=self.limit_snapshot_cycle and neutral<self.minNeutral:
-            t=0
+            self.t=0
         if neutral<self.minNeutral:
             self.minNeutral=neutral.clone()
         if neutral>self.snapshot_recovery_threshold*self.minNeutral or not loss.isfinite():
-            t=0
+            self.t=0
             self.upperEnvelope = self.upperEnvelopeCache.clone()
             self.lowerEnvelope = self.lowerEnvelopeCache.clone()
 
